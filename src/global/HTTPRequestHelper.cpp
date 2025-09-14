52bbad25e89d0d67da80cc6fa8b8a8609330dea1
mikeshadow1452 <maxicus54@gmail.com>
2025-09-14 22:39:56 +0300

    a

--- src/global/HTTPRequestHelper.cpp
+++ src/global/HTTPRequestHelper.cpp
@@ -1,131 +1,155 @@
 #include "include/global/HTTPRequestHelper.hpp"
 
-#include <QNetworkProxy>
+#include <QApplication>
+#include <QFile>
 #include <QNetworkAccessManager>
+#include <QNetworkProxy>
 #include <QNetworkReply>
 #include <QNetworkRequest>
 #include <QTimer>
-#include <QFile>
-#include <QApplication>
 
 #include "include/global/NekoGui.hpp"
 #include "include/ui/mainwindow.h"
 
 namespace NekoGui_network {
 
-    NekoHTTPResponse NetworkRequestHelper::HttpGet(const QString &url) {
-        QNetworkRequest request;
-        QNetworkAccessManager accessManager;
-        request.setUrl(url);
-        if (NekoGui::dataStore->sub_use_proxy || NekoGui::dataStore->spmode_system_proxy) {
-            QNetworkProxy p;
-            p.setType(QNetworkProxy::HttpProxy);
-            p.setHostName("127.0.0.1");
-            p.setPort(NekoGui::dataStore->inbound_socks_port);
-            accessManager.setProxy(p);
-            if (NekoGui::dataStore->started_id < 0) {
-                return NekoHTTPResponse{QObject::tr("Request with proxy but no profile started.")};
-            }
-        }
-        // Set attribute
-        request.setAttribute(QNetworkRequest::RedirectPolicyAttribute, QNetworkRequest::NoLessSafeRedirectPolicy);
-        request.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader, NekoGui::dataStore->GetUserAgent());
-        if (NekoGui::dataStore->sub_insecure) {
-            QSslConfiguration c;
-            c.setPeerVerifyMode(QSslSocket::PeerVerifyMode::VerifyNone);
-            request.setSslConfiguration(c);
-        }
-        //
-        auto _reply = accessManager.get(request);
-        connect(_reply, &QNetworkReply::sslErrors, _reply, [](const QList<QSslError> &errors) {
-            QStringList error_str;
-            for (const auto &err: errors) {
-                error_str << err.errorString();
-            }
-            MW_show_log(QString("SSL Errors: %1 %2").arg(error_str.join(","), NekoGui::dataStore->sub_insecure ? "(Ignored)" : ""));
-        });
-        // Wait for response
-        auto abortTimer = new QTimer;
-        abortTimer->setSingleShot(true);
-        abortTimer->setInterval(10000);
-        connect(abortTimer, &QTimer::timeout, _reply, &QNetworkReply::abort);
-        abortTimer->start();
-        {
-            QEventLoop loop;
-            connect(_reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
-            loop.exec();
-        }
-        if (abortTimer != nullptr) {
-            abortTimer->stop();
-            abortTimer->deleteLater();
-        }
-        //
-        auto result = NekoHTTPResponse{_reply->error() == QNetworkReply::NetworkError::NoError ? "" : _reply->errorString(),
-                                       _reply->readAll(), _reply->rawHeaderPairs()};
-        _reply->deleteLater();
-        return result;
+NekoHTTPResponse NetworkRequestHelper::HttpGet(const QString &url) {
+  QNetworkRequest request;
+  QNetworkAccessManager accessManager;
+  request.setUrl(url);
+  if (NekoGui::dataStore->sub_use_proxy ||
+      NekoGui::dataStore->spmode_system_proxy) {
+    QNetworkProxy p;
+    p.setType(QNetworkProxy::HttpProxy);
+    p.setHostName("127.0.0.1");
+    p.setPort(NekoGui::dataStore->inbound_socks_port);
+    accessManager.setProxy(p);
+    if (NekoGui::dataStore->started_id < 0) {
+      return NekoHTTPResponse{
+          QObject::tr("Request with proxy but no profile started.")};
     }
-
-    QString NetworkRequestHelper::GetHeader(const QList<QPair<QByteArray, QByteArray>> &header, const QString &name) {
-        for (const auto &p: header) {
-            if (QString(p.first).toLower() == name.toLower()) return p.second;
-        }
-        return "";
-    }
-
-    QString NetworkRequestHelper::DownloadAsset(const QString &url, const QString &fileName) {
-        QNetworkRequest request;
-        QNetworkAccessManager accessManager;
-        request.setUrl(url);
-        if (NekoGui::dataStore->spmode_system_proxy) {
-            QNetworkProxy p;
-            p.setType(QNetworkProxy::HttpProxy);
-            p.setHostName("127.0.0.1");
-            p.setPort(NekoGui::dataStore->inbound_socks_port);
-            accessManager.setProxy(p);
-            if (NekoGui::dataStore->started_id < 0) {
-                return QObject::tr("Request with proxy but no profile started.");
-            }
-        }
-
-        auto _reply = accessManager.get(request);
-        connect(_reply, &QNetworkReply::sslErrors, _reply, [](const QList<QSslError> &errors) {
+  }
+  // Set attribute
+  request.setAttribute(QNetworkRequest::RedirectPolicyAttribute,
+                       QNetworkRequest::NoLessSafeRedirectPolicy);
+  request.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader,
+                    NekoGui::dataStore->GetUserAgent());
+  if (NekoGui::dataStore->sub_insecure) {
+    QSslConfiguration c;
+    c.setPeerVerifyMode(QSslSocket::PeerVerifyMode::VerifyNone);
+    request.setSslConfiguration(c);
+  }
+  QByteArray hwid = QSysInfo::machineUniqueId();
+  QByteArray device_os = QSysInfo::productType().toUtf8();
+  QByteArray ver_os = QSysInfo::productVersion().toUtf8();
+  QByteArray device_model = QSysInfo::machineHostName().toUtf8();
+  request.setRawHeader("x-hwid", hwid);
+  request.setRawHeader("x-device-os", device_os);
+  request.setRawHeader("x-ver-os", ver_os);
+  request.setRawHeader("x-device-model", device_model);
+  //
+  auto _reply = accessManager.get(request);
+  connect(_reply, &QNetworkReply::sslErrors, _reply,
+          [](const QList<QSslError> &errors) {
             QStringList error_str;
-            for (const auto &err: errors) {
-                error_str << err.errorString();
+            for (const auto &err : errors) {
+              error_str << err.errorString();
+            }
+            MW_show_log(
+                QString("SSL Errors: %1 %2")
+                    .arg(error_str.join(","),
+                         NekoGui::dataStore->sub_insecure ? "(Ignored)" : ""));
+          });
+  // Wait for response
+  auto abortTimer = new QTimer;
+  abortTimer->setSingleShot(true);
+  abortTimer->setInterval(10000);
+  connect(abortTimer, &QTimer::timeout, _reply, &QNetworkReply::abort);
+  abortTimer->start();
+  {
+    QEventLoop loop;
+    connect(_reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+  }
+  if (abortTimer != nullptr) {
+    abortTimer->stop();
+    abortTimer->deleteLater();
+  }
+  //
+  auto result =
+      NekoHTTPResponse{_reply->error() == QNetworkReply::NetworkError::NoError
+                           ? ""
+                           : _reply->errorString(),
+                       _reply->readAll(), _reply->rawHeaderPairs()};
+  _reply->deleteLater();
+  return result;
+}
+
+QString NetworkRequestHelper::GetHeader(
+    const QList<QPair<QByteArray, QByteArray>> &header, const QString &name) {
+  for (const auto &p : header) {
+    if (QString(p.first).toLower() == name.toLower())
+      return p.second;
+  }
+  return "";
+}
+
+QString NetworkRequestHelper::DownloadAsset(const QString &url,
+                                            const QString &fileName) {
+  QNetworkRequest request;
+  QNetworkAccessManager accessManager;
+  request.setUrl(url);
+  if (NekoGui::dataStore->spmode_system_proxy) {
+    QNetworkProxy p;
+    p.setType(QNetworkProxy::HttpProxy);
+    p.setHostName("127.0.0.1");
+    p.setPort(NekoGui::dataStore->inbound_socks_port);
+    accessManager.setProxy(p);
+    if (NekoGui::dataStore->started_id < 0) {
+      return QObject::tr("Request with proxy but no profile started.");
+    }
+  }
+
+  auto _reply = accessManager.get(request);
+  connect(_reply, &QNetworkReply::sslErrors, _reply,
+          [](const QList<QSslError> &errors) {
+            QStringList error_str;
+            for (const auto &err : errors) {
+              error_str << err.errorString();
             }
             MW_show_log(QString("SSL Errors: %1").arg(error_str.join(",")));
-        });
-        connect(_reply, &QNetworkReply::downloadProgress, _reply, [&](qint64 bytesReceived, qint64 bytesTotal)
-        {
-            runOnUiThread([=]{
-                GetMainWindow()->setDownloadReport(DownloadProgressReport{fileName, bytesReceived, bytesTotal}, true);
-                GetMainWindow()->UpdateDataView();
+          });
+  connect(_reply, &QNetworkReply::downloadProgress, _reply,
+          [&](qint64 bytesReceived, qint64 bytesTotal) {
+            runOnUiThread([=] {
+              GetMainWindow()->setDownloadReport(
+                  DownloadProgressReport{fileName, bytesReceived, bytesTotal},
+                  true);
+              GetMainWindow()->UpdateDataView();
             });
-        });
-        QEventLoop loop;
-        connect(_reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
-        loop.exec();
-        runOnUiThread([=]
-        {
-            GetMainWindow()->setDownloadReport({}, false);
-            GetMainWindow()->UpdateDataView(true);
-        });
-        if(_reply->error() != QNetworkReply::NetworkError::NoError) {
-            return _reply->errorString();
-        }
+          });
+  QEventLoop loop;
+  connect(_reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+  loop.exec();
+  runOnUiThread([=] {
+    GetMainWindow()->setDownloadReport({}, false);
+    GetMainWindow()->UpdateDataView(true);
+  });
+  if (_reply->error() != QNetworkReply::NetworkError::NoError) {
+    return _reply->errorString();
+  }
 
-        auto filePath = NekoGui::GetBasePath()+ "/" + fileName;
-        auto file = QFile(filePath);
-        if (file.exists()) {
-            file.remove();
-        }
-        if (!file.open(QIODevice::WriteOnly)) {
-            return QObject::tr("Could not open file.");
-        }
-        file.write(_reply->readAll());
-        file.close();
-        return "";
-    }
+  auto filePath = NekoGui::GetBasePath() + "/" + fileName;
+  auto file = QFile(filePath);
+  if (file.exists()) {
+    file.remove();
+  }
+  if (!file.open(QIODevice::WriteOnly)) {
+    return QObject::tr("Could not open file.");
+  }
+  file.write(_reply->readAll());
+  file.close();
+  return "";
+}
 
 } // namespace NekoGui_network
